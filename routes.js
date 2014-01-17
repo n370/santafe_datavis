c55926d2ffa24ee002d19f64b67f1170287b81ea
Dylson Valente Neto <m@n370.info>
2014-01-17 17:59:00 -0300

    Minor changes

--- routes.js
+++ routes.js
@@ -1,3 +1,16 @@
+var url = require('url');
+var fs = require('fs');
+
 module.exports = function routes(req, res) {
-  res.end('Hello World');
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/public' + '/index.html').pipe(res);
+  } else if (req.url === '/edad.json') {
+    fs.createReadStream(__dirname + '/public' + '/edad.json').pipe(res);
+  } else if (req.url === '/main.js') {
+    fs.createReadStream(__dirname + '/public' + '/main.js').pipe(res);
+  } else if (req.url === '/style.css') {
+    fs.createReadStream(__dirname + '/public' + '/style.css').pipe(res);
+  } else {  
+    res.end('404', '404');
+  }
 }
