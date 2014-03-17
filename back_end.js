00f8e54fae39b1954e677a68b08cb119d24457b5
Dylson Valente Neto <dylsonvalenteneto@gmail.com>
2014-03-17 19:08:16 -0300

    Kickstarted pg connection

--- back_end.js
+++ back_end.js
@@ -6,14 +6,38 @@ Email: dvalenteneto@santafe.gov.ar
 var restify = require('restify'),
     pg = require('pg');
 
+var db_connection_string = 'postgres://developer:developer@localhost:5432/ipec_sf_2014';
+
+function respond(req, res, next){
+	pg.connect(db_connection_string, function(err, client, done) {
+	  if(err) {
+	    return console.error('Error fetching client from pool', err);
+	  }
+	  client.query('select * from entidades.departamentos', function(err, result) {
+	    //call `done()` to release the client back to the pool
+	    done();
+
+	    if(err) {
+	      return console.error('Error running query', err);
+	    }
+	    res.send(201, result.rows[0].rotulo);
+	    return next();
+	    //output: 1
+	  });
+	});
+}
+
+
 var server = restify.createServer({
 	name: 'Santa Fe DataVis'
 });
 
-server.get(/\/?.*/, restify.serveStatic({
-  directory: './public',
-  default: 'main.html'
-}));
+//server.get(/\/?.*/, restify.serveStatic({
+//  directory: './public',
+//  default: 'main.html'
+//}));
+
+server.get('/provincia', respond);
 
 server.listen(1337, function () {
 	console.log('%s listening at %s', server.name, server.url);
