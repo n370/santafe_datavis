aeffa3e611f57a395e90305901d956adb645281c
Dylson Valente Neto <dylsonvalenteneto@gmail.com>
2014-05-26 15:57:03 -0300

    Issue with async modules and function calls

--- public/scripts/views/map-view.js
+++ public/scripts/views/map-view.js
@@ -29,82 +29,6 @@ function Module(Collection,d3,topojson) {
   
   var col = Collection.mapCollection;
 
-  function initialize() {      
-
-    var geometries = this.collection.models[0].features;
-
-    function getCssPropertyNumber(selector, property) {
-      var result = new String();
-      var val = $(selector).css(property);
-      for (var i = 0; i < val.length; i++ ) {
-        var digit = parseInt(val[i]);
-        if (digit <= 9) {
-          result = result + digit;
-        } 
-      }
-      return parseInt(result);
-    }
- 
-    function getAndParse(provincia) { 
-      var id = provincia.id.toLowerCase();
-      id = id.split(' ');
-      id = id.join('_');
-      return id; 
-    }
-
-    function zooming() {
-      var translate = d3.event.translate;
-      var scale = d3.event.scale;
-      var str = "translate(" + translate + ")scale(" + scale + ")";
-      d3.select(this)
-        .attr("class", "zooming") 
-        .attr("transform", str);
-    }
-
-    function zoomed() {
-      d3.select(this)
-        .attr('class', '');
-    }
-
-    var w = $('#map-container').css('width');
-    var h = $('#map-container').css('height');
-
-    var zoom = d3.behavior.zoom()
-      .size([w,h])
-      .scaleExtent([1,20]);
-
-    zoom.on('zoom', zooming);
-    zoom.on('zoomend', zoomed);
-
-    var projection = d3.geo.mercator()
-      .center([-57,-30.5])
-      .scale(4500);
-
-    var path = d3.geo.path()
-    .projection(projection);
-
-    var departamentos = topojson.feature(geometries, geometries.objects['']);
-    
-    d3.select('#map-panel')
-      .append('svg')
-      .attr('id', 'map')
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .append('g')
-      .attr('id', 'layer')
-      .call(zoom);
-
-    d3.select('#layer').selectAll('path')
-      .data(mapas)
-      .enter()
-      .append('g')
-      .attr('class', 'departamento')
-      .style("pointer-events", "all")
-      .append("path")
-      .attr("id", getAndParse)
-      .attr("d", path);
-  } // End initialize
-  
   function render() {
   }
   
@@ -112,10 +36,86 @@ function Module(Collection,d3,topojson) {
     console.log('Maps successfully fetched!');
   }
 
-  col.fetch({
-    reset: true,
-    success: success
-  });
+  var initialize = $.when(col.fetch({ reset: true, success: success }))
+    .done(function () {
+      function initialize() {      
+
+        var geometries = this.collection.models[0].features;
+
+        function getCssPropertyNumber(selector, property) {
+          var result = new String();
+          var val = $(selector).css(property);
+          for (var i = 0; i < val.length; i++ ) {
+            var digit = parseInt(val[i]);
+            if (digit <= 9) {
+              result = result + digit;
+            } 
+          }
+          return parseInt(result);
+        }
+     
+        function getAndParse(provincia) { 
+          var id = provincia.id.toLowerCase();
+          id = id.split(' ');
+          id = id.join('_');
+          return id; 
+        }
+
+        function zooming() {
+          var translate = d3.event.translate;
+          var scale = d3.event.scale;
+          var str = "translate(" + translate + ")scale(" + scale + ")";
+          d3.select(this)
+            .attr("class", "zooming") 
+            .attr("transform", str);
+        }
+
+        function zoomed() {
+          d3.select(this)
+            .attr('class', '');
+        }
+
+        var w = $('#map-container').css('width');
+        var h = $('#map-container').css('height');
+
+        var zoom = d3.behavior.zoom()
+          .size([w,h])
+          .scaleExtent([1,20]);
+
+        zoom.on('zoom', zooming);
+        zoom.on('zoomend', zoomed);
+
+        var projection = d3.geo.mercator()
+          .center([-57,-30.5])
+          .scale(4500);
+
+        var path = d3.geo.path()
+        .projection(projection);
+
+        var departamentos = topojson.feature(geometries, geometries.objects['']);
+        
+        d3.select('#map-panel')
+          .append('svg')
+          .attr('id', 'map')
+          .attr('width', '100%')
+          .attr('height', '100%')
+          .append('g')
+          .attr('id', 'layer')
+          .call(zoom);
+
+        d3.select('#layer').selectAll('path')
+          .data(mapas)
+          .enter()
+          .append('g')
+          .attr('class', 'departamento')
+          .style("pointer-events", "all")
+          .append("path")
+          .attr("id", getAndParse)
+          .attr("d", path);
+      } // End initialize
+
+      return initialize();
+    });
 
   console.log(col); // Debugging
 
