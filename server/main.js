2443f27401fb781d9bf72a46ce45d84d1176a2f9
Dylson Valente Neto <dylsonvalenteneto@gmail.com>
2014-06-30 09:46:53 -0300

    [GRANDES CAMBIOS] Estamos intentando simplificar las cosas

--- server/main.js
+++ server/main.js
@@ -26,14 +26,11 @@
 var express = require('express'),
     server = express();
 
-var mapRoutes = require('./routes/map-router').routes;
-
 var staticFiles = express.static('./client', {
   'index': ['main.html', 'main.htm']
 });
 
 server.use('/', staticFiles);
-server.use('/maps', mapRoutes);
 
 server.listen(1337);
 console.log('HTTP server running on port 1337');
